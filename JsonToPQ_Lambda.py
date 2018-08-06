54158185c69a960cee18fc2b2b502da4b4b68a4a
dsy <daniel.s.yod@gmail.com>
2018-08-06 16:29:43 -0700

    Update JsonToPQ_Lambda.py
    Added s3fs to stream write to S3 rather than create locally/upload. Addressed IOError - out of storage.

--- JsonToPQ_Lambda.py
+++ JsonToPQ_Lambda.py
@@ -6,6 +6,7 @@ import uuid
 from fastparquet import write
 import boto3
 import gzip
+import s3fs
 #import pandas as pd #(for further DF transformation)
 #import numpy as np
 from pandas.io.json import json_normalize
@@ -15,30 +16,29 @@ outputBucket = 'outputS3Bucket'
 outputKeyspace = 'outputFolderKey/'
 
 def writePQtoS3(df):
-    #Write compressed parquet file to S3
+    #Write parquet file to S3
     #Randomized output name as function can combine several logs
-    #TODO Potentially add key to map /hour/ to folder partitions
-    bucket = outputBucket
-    name = str(uuid.uuid4())[:20]
+    #TODO (?) Potentially add key to map /hour/ to folder partitions
+    name = str(uuid.uuid4())[:15]
     filename = 'log_'+name+'.parquet.gzip'
     key = outputKeyspace+filename
-    write('/tmp/'+filename, df,compression='GZIP') #fastparquet supports SNAPPY
-    pqFile = '/tmp/'+filename
-    s3.upload_file(pqFile,bucket, Key=key)
-    print('uploaded: ',filename)
+    s3_fs = s3fs.S3FileSystem()
+    s3_fs_open = s3_fs.open
+    write(outputBucket+key, df,compression='GZIP',open_with=s3_fs_open)
+    print('wrote to S3: ',key)
 
 def transform_DF(recordList):
         #Flatten nested JSON and cast list of dicts to pandas DF
-        #Set compound column separator as '_' for Athena compatability
+        #Set separator to '_' for Athena compatability
         workingDF = json_normalize(data=recordList, sep='_')
         writePQtoS3(workingDF)
-        
+
 def lambda_handler(event, context):
     #Transfrom json.gz logs to parquet. Logs records are lines of json.
     #Single Lambda could ingest 2-3 10mb log files.
-    
+
     incomingRecords = []
-    
+
     for record in event['Records']:
         #read SQS Message to get new S3 log objects
         recDict = json.loads(record['body'])
@@ -51,14 +51,11 @@ def lambda_handler(event, context):
             data = response['Body'].read()
             unzipped = gzip.GzipFile(fileobj=StringIO(data)).read()
             lines = unzipped.split('\n')
-            linesJson = [json.loads(line) for line in lines[:-1]] #modify depending on log files (last line was empty)
-            incomingRecords = incomingRecords[:] + linesJson[:] 
+            linesJson = [json.loads(line) for line in lines[:-1]]
+            incomingRecords = incomingRecords[:] + linesJson[:]
 
         except Exception as e:
-            print('Error with: {} in bucket {}.'.format(key, bucket))
+            print('Error getting object {} from bucket {}.'.format(key, bucket))
             raise e
-            
+
     transform_DF(incomingRecords)
-
-
-            
